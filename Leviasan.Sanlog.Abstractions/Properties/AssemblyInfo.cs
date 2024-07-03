c63e4bcded5afc0add6433ea26537a412c32244f
Oleksandr Rogozin <rsashav@hotmail.com>
2024-07-03 13:06:21 +0300

    NamedFormatString as internal API

---
+++ Leviasan.Sanlog.Abstractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM components.
+// If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+[assembly: Guid("fde70942-e542-49f0-a4fd-c61263054149")]
+// Internal types are visible for unit testing.
+[assembly: InternalsVisibleTo("Leviasan.Sanlog.MSTest")]
