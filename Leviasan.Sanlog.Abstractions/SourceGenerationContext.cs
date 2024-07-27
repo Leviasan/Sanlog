7dd25961288cbc5133baf5353a580e34198b20c6
Oleksandr Rogozin <rsashav@hotmail.com>
2024-07-27 18:51:06 +0300

    ...

--- Leviasan.Sanlog.Abstractions/SourceGenerationContext.cs
+++ Leviasan.Sanlog.Abstractions/SourceGenerationContext.cs
@@ -2,6 +2,9 @@
 
 namespace Leviasan.Sanlog
 {
+    /// <summary>
+    /// Provides metadata about a set of types that is relevant to JSON serialization.
+    /// </summary>
     [JsonSerializable(typeof(LoggingEntry), GenerationMode = JsonSourceGenerationMode.Metadata)]
     [JsonSourceGenerationOptions(WriteIndented = true, DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
     internal sealed partial class SourceGenerationContext : JsonSerializerContext { }
