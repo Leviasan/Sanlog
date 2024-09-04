cbbfe270109733900508b803efcb4509542d0e8d
Oleksandr Rogozin <rsashav@hotmail.com>
2024-09-04 12:44:20 +0300

    ...

---
+++ Leviasan.Sanlog.MSTest/MessageTemplateUnitTest.cs
@@ -0,0 +1,95 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Leviasan.Sanlog.MSTest
+{
+    [TestClass]
+    public sealed class MessageTemplateUnitTest
+    {
+        private static readonly DateTime DateTimeValue = new(2024, 5, 22, 23, 56, 18);
+        private static readonly StringComparison StringComparisonValue = StringComparison.Ordinal;
+
+        [TestMethod]
+        public void FormatEmpty()
+        {
+            var messageTemplate = new MessageTemplate("DateTime: {DateTime}");
+            Assert.AreEqual("DateTime", messageTemplate.Segments[0]);
+            Assert.AreEqual("DateTime: {0}", messageTemplate.CompositeFormat.Format);
+            Assert.AreEqual("DateTime: 05/22/2024 23:56:18", messageTemplate.Format(CultureInfo.InvariantCulture, DateTimeValue));
+        }
+        [TestMethod]
+        public void FormatFormatString()
+        {
+            var messageTemplate = new MessageTemplate("DateTime: {DateTime:Y}");
+            Assert.AreEqual("DateTime", messageTemplate.Segments[0]);
+            Assert.AreEqual("DateTime: {0:Y}", messageTemplate.CompositeFormat.Format);
+            Assert.AreEqual("DateTime: 2024 May", messageTemplate.Format(CultureInfo.InvariantCulture, DateTimeValue));
+        }
+        [TestMethod]
+        public void FormatAlignmentAndFormatString()
+        {
+            var messageTemplate = new MessageTemplate("DateTime: {DateTime,-22:Y}");
+            Assert.AreEqual("DateTime", messageTemplate.Segments[0]);
+            Assert.AreEqual("DateTime: {0,-22:Y}", messageTemplate.CompositeFormat.Format);
+            Assert.AreEqual("DateTime: 2024 May              ", messageTemplate.Format(CultureInfo.InvariantCulture, DateTimeValue));
+        }
+        [TestMethod]
+        public void FormatThreeItemsWithTwoEqualsAndDifferentFormats()
+        {
+            var messageTemplate = new MessageTemplate("Year month: {DateTime:Y}. StringComparison: {StringComparison:D}. Sortable date/time: {DateTime:s}.");
+            Assert.AreEqual("DateTime", messageTemplate.Segments[0]);
+            Assert.AreEqual("StringComparison", messageTemplate.Segments[1]);
+            Assert.AreEqual("DateTime", messageTemplate.Segments[2]);
+            Assert.AreEqual("Year month: {0:Y}. StringComparison: {1:D}. Sortable date/time: {0:s}.", messageTemplate.CompositeFormat.Format);
+            Assert.AreEqual("Year month: 2024 May. StringComparison: 4. Sortable date/time: 2024-05-22T23:56:18.", messageTemplate.Format(CultureInfo.InvariantCulture, DateTimeValue, StringComparisonValue));
+        }
+        [TestMethod]
+        public void IndexComponentSingle()
+        {
+            var messageTemplate = new MessageTemplate("0x{0:X} {0:E} {0:N}");
+            Assert.AreEqual("0x7FFFFFFFFFFFFFFF 9.223372E+018 9,223,372,036,854,775,807.00", messageTemplate.Format(CultureInfo.InvariantCulture, long.MaxValue));
+            messageTemplate = new MessageTemplate("{DateTime:dddd MMMM}");
+            Assert.AreEqual(DateTime.Now.ToString("dddd MMMM", CultureInfo.InvariantCulture), messageTemplate.Format(CultureInfo.InvariantCulture, DateTime.Now));
+        }
+        [TestMethod]
+        public void IndexComponentNotOrdered()
+        {
+            var format = "{1} {0} {2}";
+            var messageTemplate = new MessageTemplate(format);
+            Assert.AreEqual("1", messageTemplate.Segments[0]);
+            Assert.AreEqual("0", messageTemplate.Segments[1]);
+            Assert.AreEqual("2", messageTemplate.Segments[2]);
+            Assert.AreEqual(string.Format(CultureInfo.InvariantCulture, CompositeFormat.Parse(format), 0, 1, 2), messageTemplate.Format(CultureInfo.InvariantCulture, 0, 1, 2));
+        }
+        [TestMethod]
+        public void InvalidInputStringFormatException()
+        {
+            var formatException = Assert.ThrowsException<FormatException>(() => new MessageTemplate("{1} {InvalidName} {2}"));
+            Assert.AreEqual("The input string was not in the correct format. Fail to parse near offset 5. The mixed argument names are not supported.", formatException.Message);
+            formatException = Assert.ThrowsException<FormatException>(() => new MessageTemplate("{InvalidName} {0} {2}"));
+            Assert.AreEqual("The input string was not in the correct format. Fail to parse near offset 15. The mixed argument names are not supported.", formatException.Message);
+            formatException = Assert.ThrowsException<FormatException>(() => new MessageTemplate("{1} {0} {InvalidName}"));
+            Assert.AreEqual("The input string was not in the correct format. Fail to parse near offset 9. The mixed argument names are not supported.", formatException.Message);
+            formatException = Assert.ThrowsException<FormatException>(() => new MessageTemplate("{1} {0} {2S}"));
+            Assert.AreEqual("The input string was not in the correct format. Fail to parse near offset 9. The mixed argument names are not supported.", formatException.Message);
+        }
+        [TestMethod]
+        public void AlignmentComponent()
+        {
+            var messageTemplate = new MessageTemplate("{0,-20} {1,5:N1}");
+            Assert.AreEqual("Adam                  40.0", messageTemplate.Format(CultureInfo.InvariantCulture, "Adam", 40));
+        }
+        [TestMethod]
+        public void EscapingBraces()
+        {
+            var messageTemplate = new MessageTemplate("{{{0:D}}}");
+            Assert.AreEqual("{6324}", messageTemplate.Format(CultureInfo.InvariantCulture, 6324));
+        }
+        [TestMethod]
+        public void MixedNamingConvention()
+        {
+            var messageTemplate = new MessageTemplate("{A1} {0A} {CC} {A1,5:X2}");
+            Assert.AreEqual("15 1 2    0F", messageTemplate.Format(CultureInfo.InvariantCulture, 15, 1, 2));
+        }
+    }
+}
