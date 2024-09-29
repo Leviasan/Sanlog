de5b05fdb5de133248c914f8b612c73fce143fa4
Oleksandr Rogozin <rsashav@hotmail.com>
2024-09-29 04:06:51 +0300

    ...

---
+++ Sanlog.EFCore.Server/TenantClientConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Sanlog.EFCore.Server
+{
+    /// <summary>
+    /// The model configuration of the <see cref="TenantClient"/> model.
+    /// </summary>
+    internal sealed class TenantClientConfiguration : IEntityTypeConfiguration<TenantClient>
+    {
+        /// <inheritdoc/>
+        public void Configure(EntityTypeBuilder<TenantClient> builder)
+        {
+            _ = builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            _ = builder.Property(x => x.ClientName).IsRequired(true).IsUnicode(true);
+            _ = builder.Property(x => x.ClientDescription).IsRequired(false).IsUnicode(true);
+            _ = builder.HasIndex(x => x.ClientName).IsUnique();
+        }
+    }
+}
---
+++ Sanlog.EFCore.Server/TenantDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Sanlog.EFCore.Server
+{
+    public sealed class TenantDbContext : DbContext
+    {
+        [RequiresDynamicCode("EF Core isn't fully compatible with NativeAOT, and running the application may generate unexpected runtime failures.")]
+        [RequiresUnreferencedCode("EF Core isn't fully compatible with trimming, and running the application may generate unexpected runtime failures. Some specific coding pattern are usually required to make trimming work properly, see https://aka.ms/efcore-docs-trimming for more details.")]
+        public TenantDbContext(DbContextOptions<TenantDbContext> options) : base(options) { }
+
+        public DbSet<TenantClient> Clients => Set<TenantClient>();
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Debug.Assert(optionsBuilder is not null);
+            _ = optionsBuilder.UseLoggerFactory(NullLoggerFactory.Instance);
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Debug.Assert(modelBuilder is not null);
+            _ = modelBuilder.ApplyConfiguration(new TenantClientConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
