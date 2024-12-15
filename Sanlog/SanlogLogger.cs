8b7a772942c5edeccaace65d51fb2b73bb03ba56
Rogozin Oleksandr <rsashav@hotmail.com>
2024-12-16 00:59:30 +0200

    ...

--- Sanlog/SanlogLogger.cs
+++ Sanlog/SanlogLogger.cs
@@ -34,7 +34,11 @@ namespace Sanlog
         /// <inheritdoc/>
         public IDisposable? BeginScope<TState>(TState state) where TState : notnull => _provider.ExternalScopeProvider?.Push(state);
         /// <inheritdoc/>
-        public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None && _provider.Options.AppId != Guid.Empty && _provider.Options.TenantId != Guid.Empty;
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            var options = _provider.Options;
+            return logLevel != LogLevel.None && options.AppId != Guid.Empty && options.TenantId != Guid.Empty;
+        }
         /// <inheritdoc/>
         /// <exception cref="ArgumentNullException">The <paramref name="formatter"/> is <see langword="null"/>.</exception>
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
@@ -42,17 +46,18 @@ namespace Sanlog
             ArgumentNullException.ThrowIfNull(formatter);
             if (IsEnabled(logLevel))
             {
+                var options = _provider.Options;
                 var formattedLogValuesFormatter = new FormattedLogValuesFormatter(state is IReadOnlyList<KeyValuePair<string, object?>> list ? list.ToDictionary() : [])
                 {
-                    SensitiveConfiguration = _provider.Options.SensitiveConfiguration
+                    SensitiveConfiguration = options.SensitiveConfiguration
                 };
                 var logEntryId = Guid.NewGuid();
                 var loggingEntry = new LoggingEntry
                 {
-                    TenantId = _provider.Options.TenantId,
+                    TenantId = options.TenantId,
                     Id = logEntryId,
-                    AppId = _provider.Options.AppId,
-                    Version = _provider.Options.OnRetrieveVersion?.Invoke(),
+                    AppId = options.AppId,
+                    Version = options.OnRetrieveVersion?.Invoke(),
                     DateTime = DateTime.Now,
                     LoggingLevelId = (int)logLevel,
                     Category = _category,
@@ -60,11 +65,11 @@ namespace Sanlog
                     EventName = eventId.Name,
                     Message = formattedLogValuesFormatter.ContainsKey(FormattedLogValuesFormatter.OriginalFormat) ? formattedLogValuesFormatter.ToString() : formatter.Invoke(state, exception),
                     Properties = formattedLogValuesFormatter.ToStringDictionary(),
-                    Scopes = GetScopeInformation(CultureInfo.InvariantCulture, state, logEntryId, _provider.Options, _provider.ExternalScopeProvider),
+                    Scopes = GetScopeInformation(CultureInfo.InvariantCulture, state, logEntryId, options, _provider.ExternalScopeProvider),
                     Errors = exception is not null
                         ? exception is not AggregateException aggregateException
-                            ? [GetErrorInformation(_provider.Options, Guid.NewGuid(), exception, logEntryId, null)]
-                            : aggregateException.Flatten().InnerExceptions.Select(innerException => GetErrorInformation(_provider.Options, Guid.NewGuid(), innerException, logEntryId, null)).ToList()
+                            ? [GetErrorInformation(options, Guid.NewGuid(), exception, logEntryId, null)]
+                            : aggregateException.Flatten().InnerExceptions.Select(innerException => GetErrorInformation(options, Guid.NewGuid(), innerException, logEntryId, null)).ToList()
                         : []
                 };
                 _provider.AddMessage(loggingEntry);
@@ -81,7 +86,7 @@ namespace Sanlog
                     Type = exception.GetType().FullName!,
                     Message = exception.Message,
                     HResult = exception.HResult,
-                    Data = ParseData(exception.Data),
+                    Data = ParseDictionary(exception.Data),
                     StackTrace = exception.StackTrace,
                     Source = exception.Source,
                     HelpLink = exception.HelpLink,
@@ -95,7 +100,7 @@ namespace Sanlog
                         : []
                 };
 
-                static Dictionary<string, string?>? ParseData(IDictionary dictionary)
+                static Dictionary<string, string?>? ParseDictionary(IDictionary dictionary)
                 {
                     Debug.Assert(dictionary is not null);
                     if (dictionary.Count == 0)
