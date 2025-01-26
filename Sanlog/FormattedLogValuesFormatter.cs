e228e3eef3067e3afea423ab4eb41a12f7396074
Rogozin Oleksandr <rsashav@hotmail.com>
2025-01-26 19:34:02 +0200

    ...

--- Sanlog/FormattedLogValuesFormatter.cs
+++ Sanlog/FormattedLogValuesFormatter.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Sanlog
@@ -9,12 +12,24 @@ namespace Sanlog
     /// <summary>
     /// Represents the formatter that supports custom formatting of Microsoft.Extensions.Logging.FormattedLogValues object.
     /// </summary>
-    public sealed class FormattedLogValuesFormatter : SensitiveFormatter
+    public sealed class FormattedLogValuesFormatter : IFormatProvider, ICustomFormatter
     {
+        /// <summary>
+        /// The format string is used to redact sensitive values.
+        /// </summary>
+        public const string FormatRedacted = "R";
+        /// <summary>
+        /// The format string is used to serialize public properties to a string representation.
+        /// </summary>
+        public const string FormatSerialize = "S";
         /// <summary>
         /// The message format that represents a null value.
         /// </summary>
         public const string NullValue = "(null)";
+        /// <summary>
+        /// The message format that represents a redacted value.
+        /// </summary>
+        public const string RedactedValue = "[Redacted]";
 
         /// <summary>
         /// The configuration of the formatter.
@@ -38,14 +53,125 @@ namespace Sanlog
             };
             set => _configuration = value;
         }
+        /// <summary>
+        /// Gets or sets the formatting culture.
+        /// </summary>
+        public CultureInfo? CultureInfo { get; set; }
 
         /// <inheritdoc/>
-        public override string Format(string? format, object? arg, IFormatProvider? formatProvider)
-        {
-            const string EmptyArray = "[]";
-            return Equals(formatProvider) && string.IsNullOrEmpty(format) && TryCustomFormat(arg, formatProvider, base.Format, Configuration, out var stringValue) ? stringValue : base.Format(format, arg, formatProvider);
+        public object? GetFormat(Type? formatType) => formatType == typeof(ICustomFormatter) ? this : CultureInfo?.GetFormat(formatType);
 
-            static bool TryCustomFormat(object? value, IFormatProvider? formatProvider, Func<string?, object?, IFormatProvider?, string> formatter, FormattedLogValuesFormatterOptions configuration, [NotNullWhen(true)] out string? stringValue)
+        /// <inheritdoc/>
+        public string Format(string? format, object? arg, IFormatProvider? formatProvider)
+        {
+            Configuration.MakeReadOnly(); // Disallows to change configuration while formatting
+
+            if (Equals(formatProvider))
+            {
+                if (string.IsNullOrEmpty(format))
+                {
+                    if (OverrideToString(arg, formatProvider, Configuration) is string stringValue && !string.IsNullOrEmpty(stringValue))
+                    {
+                        return stringValue;
+                    }
+                }
+                else
+                {
+                    if (format.Equals(FormatRedacted, StringComparison.Ordinal))
+                    {
+                        return RedactedValue;
+                    }
+                    if (format.Equals(FormatSerialize, StringComparison.Ordinal) && arg is not null)
+                    {
+                        var stringValue = arg switch
+                        {
+                            string str => str, // string implements IEnumerable so must be process before
+                            IDictionary dictionary => IDictionaryToString(dictionary, formatProvider, formatter, configuration), // IDictionary implements IEnumerable so must be process before
+                            IEnumerable enumerable => IEnumerableToString(enumerable, formatProvider, formatter, configuration),
+                            _ => SerializeObject(arg, formatProvider)
+                        };
+
+                        /*
+                        var type = arg.GetType();
+                        var stringValue = arg switch
+                        {
+                            string str => str, // string implements IEnumerable so must be process before
+                            IDictionary dictionary => IDictionaryToString(dictionary, formatProvider, formatter, configuration), // IDictionary implements IEnumerable so must be process before
+                            IEnumerable enumerable => IEnumerableToString(enumerable, formatProvider, formatter, configuration),
+                            _ => throw new NotImplementedException()
+                        };
+                        */
+
+                        return Serialize(arg, arg.GetType(), this);
+                    }
+                }
+            }
+            return DefaultFallback(format, arg, Equals(formatProvider) ? CultureInfo : formatProvider);
+
+            static string DefaultFallback(string? format, object? arg, IFormatProvider? formatProvider)
+            {
+                return arg switch
+                {
+                    IFormattable formattable => formattable.ToString(format, formatProvider),
+                    _ => Convert.ToString(arg, formatProvider) ?? string.Empty
+                };
+            };
+            static string? OverrideToString(object? arg, IFormatProvider? formatProvider, FormattedLogValuesFormatterOptions configuration)
+            {
+                return arg switch
+                {
+                    DateTime dateTime => dateTime.ToString(configuration.DateTimeFormat, formatProvider),
+                    DateTimeOffset dateTimeOffset => dateTimeOffset.ToString(configuration.DateTimeOffsetFormat, formatProvider),
+                    Enum @enum => @enum.ToString(configuration.EnumFormat),
+                    float binary32 => binary32.ToString(configuration.SingleFormat, formatProvider),
+                    double binary64 => binary64.ToString(configuration.DoubleFormat, formatProvider),
+                    null => NullValue,
+                    _ => null
+                };
+            }
+            static string SerializeObject(object value, IFormatProvider? provider)
+            {
+                const string EmptyObject = "{}";
+                const string SimpleFormat = "{0}";
+                const string RedactedFormat = "{0:R}";
+
+                StringBuilder? stringBuilder = null;
+                var type = value.GetType();
+                var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+                for (var index = 0; index < properties.Length; ++index)
+                {
+                    var property = properties[index];
+                    stringBuilder = stringBuilder is null ? new StringBuilder(256).Append('{') : stringBuilder;
+                    _ = stringBuilder
+                        .Append(' ')
+                        .Append(property.Name)
+                        .Append(" = ")
+                        .Append(string.Format(provider, property.IsDefined(typeof(SensitiveAttribute)) ? RedactedFormat : SimpleFormat, property.GetValue(value)))
+                        .Append(index < properties.Length - 1 ? ',' : ' ');
+                }
+                return stringBuilder?.Append('}').ToString() ?? EmptyObject;
+            }
+            static string IDictionarySerialize(IDictionary dictionary, IFormatProvider formatProvider)
+            {
+                const string EmptyArray = "[]";
+
+                var first = true;
+                StringBuilder? stringBuilder = null;
+                foreach (DictionaryEntry entry in dictionary)
+                {
+                    stringBuilder = first ? new StringBuilder(256).Append('[') : stringBuilder!.Append(", ");
+                    stringBuilder = stringBuilder.Append(formatProvider, $"[{Serialize(entry.Key, entry.Key.GetType(), formatProvider)}, {ObjectToString(entry.Value, formatProvider, formatter, configuration)}]");
+                    first = false;
+                }
+                return stringBuilder?.Append(']').ToString() ?? EmptyArray;
+            }
+            // ----------------------------------------- NULLABLE FORMAT -----------------------------------------
+            /*
+             *                     string str => str, // string implements IEnumerable so must be process before
+                    IDictionary dictionary => IDictionarySerialize(dictionary, formatProvider, formatter, configuration), // IDictionary implements IEnumerable so must be process before
+                    IEnumerable enumerable => IEnumerableToString(enumerable, formatProvider, formatter, configuration),
+
+            static bool TryCustomFormat(object? value, IFormatProvider? formatProvider, FormattedLogValuesFormatterOptions configuration, [NotNullWhen(true)] out string? stringValue)
             {
                 stringValue = value switch
                 {
@@ -54,28 +180,15 @@ namespace Sanlog
                     Enum @enum => @enum.ToString(configuration.EnumFormat),
                     float binary32 => binary32.ToString(configuration.SingleFormat, formatProvider),
                     double binary64 => binary64.ToString(configuration.DoubleFormat, formatProvider),
-                    string str => str, // string implements IEnumerable so must be process before
-                    IDictionary dictionary => IDictionaryToString(dictionary, formatProvider, formatter, configuration), // IDictionary implements IEnumerable so must be process before
-                    IEnumerable enumerable => IEnumerableToString(enumerable, formatProvider, formatter, configuration),
+                  
                     null => NullValue,
                     _ => null
                 };
                 return !string.IsNullOrEmpty(stringValue);
             }
-            static string IDictionaryToString(IDictionary dictionary, IFormatProvider? formatProvider, Func<string?, object?, IFormatProvider?, string> formatter, FormattedLogValuesFormatterOptions configuration)
-            {
-                var first = true;
-                StringBuilder? stringBuilder = null;
-                foreach (DictionaryEntry entry in dictionary)
-                {
-                    stringBuilder = first ? new StringBuilder(256).Append('[') : stringBuilder!.Append(", ");
-                    stringBuilder = stringBuilder.Append(formatProvider, $"[{ObjectToString(entry.Key, formatProvider, formatter, configuration)}, {ObjectToString(entry.Value, formatProvider, formatter, configuration)}]");
-                    first = false;
-                }
-                return stringBuilder?.Append(']').ToString() ?? EmptyArray;
-            }
-            static string ObjectToString(object? value, IFormatProvider? formatProvider, Func<string?, object?, IFormatProvider?, string> formatter, FormattedLogValuesFormatterOptions configuration)
-                => TryCustomFormat(value, formatProvider, formatter, configuration, out var stringValue) ? stringValue : formatter.Invoke(null, value, formatProvider);
+            */
+            /*
+
             static string IEnumerableToString(IEnumerable enumerable, IFormatProvider? formatProvider, Func<string?, object?, IFormatProvider?, string> formatter, FormattedLogValuesFormatterOptions configuration)
             {
                 var first = true;
@@ -88,6 +201,10 @@ namespace Sanlog
                 }
                 return stringBuilder?.Append(']').ToString() ?? EmptyArray;
             }
+            static string ObjectToString(object? value, IFormatProvider? formatProvider, Func<string?, object?, IFormatProvider?, string> formatter, FormattedLogValuesFormatterOptions configuration)
+                => TryCustomFormat(value, formatProvider, formatter, configuration, out var stringValue) ? stringValue : formatter.Invoke(null, value, formatProvider);
+            */
+            // ----------------------------------------- NULLABLE FORMAT -----------------------------------------
         }
     }
 }
--- Sanlog/FormattedLogValuesFormatterOptions.cs
+++ Sanlog/FormattedLogValuesFormatterOptions.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading;
 
 namespace Sanlog
 {
@@ -34,6 +40,11 @@ namespace Sanlog
         /// </summary>
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private string? _doubleFormat;
+        /// <summary>
+        /// The sensitive properties.
+        /// </summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly Dictionary<Type, HashSet<string>> _dictionary = [];
 
         /// <summary>
         /// Gets a value indicating whether the configuration is read-only.
@@ -111,6 +122,25 @@ namespace Sanlog
         }
 
         /// <summary>
+        /// Registers a property whose value belongs to sensitive data.
+        /// </summary>
+        /// <param name="type">The sensitive key type.</param>
+        /// <param name="property">The property whose value belongs to sensitive data.</param>
+        /// <returns><see langword="true"/> if the element is added to the collection; <see langword="false"/> if the element is already present.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="property"/> is <see langword="null"/>.</exception>
+        /// <exception cref="InvalidOperationException">The configuration is read-only.</exception>
+        public bool AddSensitive(Type type, string property)
+        {
+            var d = Mapper<FormatException>.GetProperty(x => x.StackTrace);
+            var e = AddSensitive<FormatException, string>(x => x.HResult);
+
+
+
+            CheckReadOnly(); // InvalidOperationException
+            ArgumentNullException.ThrowIfNull(property);
+            return _dictionary.TryGetValue(type, out var hashset) ? hashset.Add(property) : _dictionary.TryAdd(type, [property]);
+        }
+        /// <summary>
         /// Makes the configuration read-only.
         /// </summary>
         public void MakeReadOnly() => IsReadOnly = true;
@@ -122,6 +152,36 @@ namespace Sanlog
         {
             if (IsReadOnly)
                 throw new InvalidOperationException("The configuration is read-only.");
+        }
+
+        internal bool AddSensitive<T, P>(Expression<Func<T, P>> expression)
+        {
+            var ex = new FormatException();
+            var c = InstanceMapper.GetProperty(ex, x => x.HResult);
+
+            var e = ex.GetProperty(x => x.StackTrace);
+
+            var p = Mapper<T>.GetProperty(expression);
+            return AddSensitive(typeof(T), p.Name);
+        }
+    }
+
+    internal static class Mapper<T>
+    {
+        public static PropertyInfo GetProperty<P>(Expression<Func<T, P>> expression)
+        {
+            return expression.Body is MemberExpression member && member.Member is PropertyInfo property
+                ? property
+                : throw new ArgumentException("Expression is not a Property", nameof(expression));
+        }
+    }
+    internal static class InstanceMapper
+    {
+        public static PropertyInfo GetProperty<T, P>(this T _, Expression<Func<T, P>> expression)
+        {
+            return expression.Body is MemberExpression member && member.Member is PropertyInfo property
+                ? property
+                : throw new ArgumentException("Expression is not a Property", nameof(expression));
         }
     }
 }
