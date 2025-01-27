fbaa0e59a692ff649fa0dca613a052055d93164a
Rogozin Oleksandr <rsashav@hotmail.com>
2025-01-27 11:41:20 +0200

    ...

--- Sanlog/FormattedLogValuesFormatter.cs
+++ Sanlog/FormattedLogValuesFormatter.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Reflection;
-using System.Runtime.Serialization;
 using System.Text;
 
 namespace Sanlog
@@ -64,6 +63,7 @@ namespace Sanlog
         /// <inheritdoc/>
         public string Format(string? format, object? arg, IFormatProvider? formatProvider)
         {
+
             Configuration.MakeReadOnly(); // Disallows to change configuration while formatting
 
             if (Equals(formatProvider))
@@ -83,26 +83,7 @@ namespace Sanlog
                     }
                     if (format.Equals(FormatSerialize, StringComparison.Ordinal) && arg is not null)
                     {
-                        var stringValue = arg switch
-                        {
-                            string str => str, // string implements IEnumerable so must be process before
-                            IDictionary dictionary => IDictionaryToString(dictionary, formatProvider, formatter, configuration), // IDictionary implements IEnumerable so must be process before
-                            IEnumerable enumerable => IEnumerableToString(enumerable, formatProvider, formatter, configuration),
-                            _ => SerializeObject(arg, formatProvider)
-                        };
-
-                        /*
-                        var type = arg.GetType();
-                        var stringValue = arg switch
-                        {
-                            string str => str, // string implements IEnumerable so must be process before
-                            IDictionary dictionary => IDictionaryToString(dictionary, formatProvider, formatter, configuration), // IDictionary implements IEnumerable so must be process before
-                            IEnumerable enumerable => IEnumerableToString(enumerable, formatProvider, formatter, configuration),
-                            _ => throw new NotImplementedException()
-                        };
-                        */
-
-                        return Serialize(arg, arg.GetType(), this);
+                        return Serialize(arg, formatProvider, Configuration);
                     }
                 }
             }
@@ -129,94 +110,66 @@ namespace Sanlog
                     _ => null
                 };
             }
-            static string Serialize(object arg, IFormatProvider? provider)
-            {
-                return arg switch
-                {
-                    string str => str, // string implements IEnumerable so must be process before
-                    IDictionary dictionary => IDictionaryToString(dictionary, formatProvider, formatter, configuration), // IDictionary implements IEnumerable so must be process before
-                    IEnumerable enumerable => IEnumerableToString(enumerable, formatProvider, formatter, configuration),
-                    _ => SerializeObject(arg, formatProvider)
-                };
-            }
-
-
-            static string SerializeObject(object value, IFormatProvider? provider)
-            {
-                const string EmptyObject = "{}";
-                const string SimpleFormat = "{0}";
-                const string RedactedFormat = "{0:R}";
-
-                StringBuilder? stringBuilder = null;
-                var type = value.GetType();
-                var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
-                for (var index = 0; index < properties.Length; ++index)
-                {
-                    var property = properties[index];
-                    stringBuilder = stringBuilder is null ? new StringBuilder(256).Append('{') : stringBuilder;
-                    _ = stringBuilder
-                        .Append(' ')
-                        .Append(property.Name)
-                        .Append(" = ")
-                        .Append(string.Format(provider, property.IsDefined(typeof(SensitiveAttribute)) ? RedactedFormat : SimpleFormat, property.GetValue(value)))
-                        .Append(index < properties.Length - 1 ? ',' : ' ');
-                }
-                return stringBuilder?.Append('}').ToString() ?? EmptyObject;
-            }
-            static string IDictionarySerialize(IDictionary dictionary, IFormatProvider formatProvider)
+            static string Serialize(object? arg, IFormatProvider? formatProvider, FormattedLogValuesFormatterOptions configuration)
             {
                 const string EmptyArray = "[]";
 
-                var first = true;
-                StringBuilder? stringBuilder = null;
-                foreach (DictionaryEntry entry in dictionary)
+                return OverrideFormat(arg, formatProvider, configuration) is string stringValue && !string.IsNullOrEmpty(stringValue) ? stringValue : arg switch
                 {
-                    stringBuilder = first ? new StringBuilder(256).Append('[') : stringBuilder!.Append(", ");
-                    stringBuilder = stringBuilder.Append(formatProvider, $"[{Serialize(entry.Key, entry.Key.GetType(), formatProvider)}, {ObjectToString(entry.Value, formatProvider, formatter, configuration)}]");
-                    first = false;
-                }
-                return stringBuilder?.Append(']').ToString() ?? EmptyArray;
-            }
-            // ----------------------------------------- NULLABLE FORMAT -----------------------------------------
-            /*
-             *                     string str => str, // string implements IEnumerable so must be process before
-                    IDictionary dictionary => IDictionarySerialize(dictionary, formatProvider, formatter, configuration), // IDictionary implements IEnumerable so must be process before
-                    IEnumerable enumerable => IEnumerableToString(enumerable, formatProvider, formatter, configuration),
-
-            static bool TryCustomFormat(object? value, IFormatProvider? formatProvider, FormattedLogValuesFormatterOptions configuration, [NotNullWhen(true)] out string? stringValue)
-            {
-                stringValue = value switch
-                {
-                    DateTime dateTime => dateTime.ToString(configuration.DateTimeFormat, formatProvider),
-                    DateTimeOffset dateTimeOffset => dateTimeOffset.ToString(configuration.DateTimeOffsetFormat, formatProvider),
-                    Enum @enum => @enum.ToString(configuration.EnumFormat),
-                    float binary32 => binary32.ToString(configuration.SingleFormat, formatProvider),
-                    double binary64 => binary64.ToString(configuration.DoubleFormat, formatProvider),
-                  
+                    string str => str, // string implements IEnumerable so must be process before
+                    IDictionary dictionary => SerializeDictionary(dictionary, formatProvider, configuration), // IDictionary implements IEnumerable so must be process before
+                    IEnumerable enumerable => SerializeEnumerable(enumerable, formatProvider, configuration),
                     null => NullValue,
-                    _ => null
+                    _ => SerializeObject(arg, formatProvider)
                 };
-                return !string.IsNullOrEmpty(stringValue);
-            }
-            */
-            /*
 
-            static string IEnumerableToString(IEnumerable enumerable, IFormatProvider? formatProvider, Func<string?, object?, IFormatProvider?, string> formatter, FormattedLogValuesFormatterOptions configuration)
-            {
-                var first = true;
-                StringBuilder? stringBuilder = null;
-                foreach (var value in enumerable)
+                static string SerializeDictionary(IDictionary dictionary, IFormatProvider? formatProvider, FormattedLogValuesFormatterOptions configuration)
                 {
-                    stringBuilder = first ? new StringBuilder(256).Append('[') : stringBuilder!.Append(", ");
-                    stringBuilder = stringBuilder.Append(ObjectToString(value, formatProvider, formatter, configuration));
-                    first = false;
+                    var first = true;
+                    StringBuilder? stringBuilder = null;
+                    foreach (DictionaryEntry entry in dictionary)
+                    {
+                        stringBuilder = first ? new StringBuilder(256).Append('[') : stringBuilder!.Append(", ");
+                        stringBuilder = stringBuilder.Append(formatProvider, $"[{Serialize(entry.Key, formatProvider, configuration)}, {Serialize(entry.Value, formatProvider, configuration)}]");
+                        first = false;
+                    }
+                    return stringBuilder?.Append(']').ToString() ?? EmptyArray;
                 }
-                return stringBuilder?.Append(']').ToString() ?? EmptyArray;
+                static string SerializeEnumerable(IEnumerable enumerable, IFormatProvider? formatProvider, FormattedLogValuesFormatterOptions configuration)
+                {
+                    var first = true;
+                    StringBuilder? stringBuilder = null;
+                    foreach (var value in enumerable)
+                    {
+                        stringBuilder = first ? new StringBuilder(256).Append('[') : stringBuilder!.Append(", ");
+                        stringBuilder = stringBuilder.Append(Serialize(value, formatProvider, configuration));
+                        first = false;
+                    }
+                    return stringBuilder?.Append(']').ToString() ?? EmptyArray;
+                }
+                static string SerializeObject(object value, IFormatProvider? provider)
+                {
+                    const string EmptyObject = "{}";
+                    const string SimpleFormat = "{0}";
+                    const string RedactedFormat = "{0:R}";
+
+                    StringBuilder? stringBuilder = null;
+                    var type = value.GetType();
+                    var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+                    for (var index = 0; index < properties.Length; ++index)
+                    {
+                        var property = properties[index];
+                        stringBuilder = stringBuilder is null ? new StringBuilder(256).Append('{') : stringBuilder;
+                        _ = stringBuilder
+                            .Append(' ')
+                            .Append(property.Name)
+                            .Append(" = ")
+                            .Append(string.Format(provider, property.IsDefined(typeof(SensitiveAttribute)) ? RedactedFormat : SimpleFormat, property.GetValue(value)))
+                            .Append(index < properties.Length - 1 ? ',' : ' ');
+                    }
+                    return stringBuilder?.Append('}').ToString() ?? EmptyObject;
+                }
             }
-            static string ObjectToString(object? value, IFormatProvider? formatProvider, Func<string?, object?, IFormatProvider?, string> formatter, FormattedLogValuesFormatterOptions configuration)
-                => TryCustomFormat(value, formatProvider, formatter, configuration, out var stringValue) ? stringValue : formatter.Invoke(null, value, formatProvider);
-            */
-            // ----------------------------------------- NULLABLE FORMAT -----------------------------------------
         }
     }
 }
