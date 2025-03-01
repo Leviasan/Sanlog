3103f7523f103c0c8b331a314e72ed0572f8edbe
Rogozin Oleksandr <rsashav@hotmail.com>
2025-03-01 20:03:31 +0200

    ...

--- Sanlog.MSTest/MessageBrokerUnitTest.cs
+++ Sanlog.MSTest/MessageBrokerUnitTest.cs
@@ -5,15 +5,27 @@ namespace Sanlog.MSTest
     public sealed class MessageBrokerUnitTest
     {
         [TestMethod]
-        public void Method()
+        public async Task Method()
         {
+            var handler = new MessageHandler();
             using var broker = new MessageBroker();
-            Assert.IsTrue(broker.Register(typeof(object), new MessageHandler()));
+            await broker.StartAsync(CancellationToken.None);
+            Assert.IsTrue(broker.Register(typeof(int), handler));
+            Assert.IsTrue(broker.SendMessage(int.MaxValue));
+            await broker.StopAsync(TimeSpan.FromSeconds(5), CancellationToken.None);
+            Assert.AreEqual(int.MaxValue, handler.LastMessage);
         }
-
         private sealed class MessageHandler : IMessageHandler
         {
-            public ValueTask HandleAsync(object? message, CancellationToken cancellationToken) => ValueTask.CompletedTask;
+            public object? LastMessage { get; private set; }
+            public int Counter { get; private set; }
+
+            public ValueTask HandleAsync(object? message, CancellationToken cancellationToken)
+            {
+                ++Counter;
+                LastMessage = message;
+                return ValueTask.CompletedTask;
+            }
         }
     }
 }
