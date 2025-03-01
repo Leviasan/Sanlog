071d996f0d8235e6c2492b4bf3ed7caf26ea7db4
Rogozin Oleksandr <rsashav@hotmail.com>
2025-03-01 19:16:08 +0200

    ...

---
+++ Sanlog.MSTest/MessageBrokerUnitTest.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Sanlog.MSTest
+{
+    [TestClass]
+    public sealed class MessageBrokerUnitTest
+    {
+        [TestMethod]
+        public void Method()
+        {
+            using var broker = new MessageBroker();
+            Assert.IsTrue(broker.Register(typeof(object), new MessageHandler()));
+        }
+
+        private sealed class MessageHandler : IMessageHandler
+        {
+            public ValueTask HandleAsync(object? message, CancellationToken cancellationToken) => ValueTask.CompletedTask;
+        }
+    }
+}
