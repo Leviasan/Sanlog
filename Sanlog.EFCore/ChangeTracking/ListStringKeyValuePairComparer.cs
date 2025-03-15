189d977bd0342f0dd44cfc2755b090d057afa40d
Rogozin Oleksandr <rsashav@hotmail.com>
2025-03-15 18:56:46 +0200

    ...

--- Sanlog.EFCore/ChangeTracking/ListStringKeyValuePairComparer.cs
+++ Sanlog.EFCore/ChangeTracking/ListStringKeyValuePairComparer.cs
@@ -11,15 +11,15 @@ namespace Sanlog.EntityFrameworkCore.ChangeTracking
     /// where T is <see cref="KeyValuePair{TKey, TValue}"/> where TKey and TValue are <see cref="string"/>.
     /// </summary>
     [SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes", Justification = "Instantiated via reflection")]
-    internal sealed class ListStringKeyValuePairComparer : ValueComparer<IReadOnlyList<KeyValuePair<string, string?>>>
+    internal sealed class ListStringKeyValuePairComparer : ValueComparer<IReadOnlyList<KeyValuePair<string, string?>>?>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ListStringKeyValuePairComparer"/> class.
         /// </summary>
         public ListStringKeyValuePairComparer() : base(
             equalsExpression: static (x, y) => x != null && y != null && x.SequenceEqual(y),
-            hashCodeExpression: static x => x.Aggregate(0, (hash, value) => HashCode.Combine(hash, value.GetHashCode())),
-            snapshotExpression: static x => x.ToList())
+            hashCodeExpression: static x => x != null ? x.Aggregate(0, (hash, value) => HashCode.Combine(hash, value.GetHashCode())) : 0,
+            snapshotExpression: static x => x != null ? x.ToList() : null)
         { }
     }
 }
