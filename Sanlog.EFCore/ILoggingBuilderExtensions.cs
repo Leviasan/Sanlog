0885f6b7ad84078578fb9e7b120b4ab7ca685b06
Rogozin Oleksandr <rsashav@hotmail.com>
2025-03-15 21:42:19 +0200

    ...

--- Sanlog.EFCore/ILoggingBuilderExtensions.cs
+++ Sanlog.EFCore/ILoggingBuilderExtensions.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Configuration;
-using Sanlog.Formatters;
 
 namespace Sanlog.EntityFrameworkCore
 {
