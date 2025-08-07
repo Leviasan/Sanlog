8d0ce0980ce5cd804cd51eaa65c4a9552a3f8b99
oleksandr.rogozin <rsashav@hotmail.com>
2025-08-07 12:55:18 +0300

    docs: changed summary

--- Sanlog.Abstractions/Formatters/IValueFormatter.cs
+++ Sanlog.Abstractions/Formatters/IValueFormatter.cs
@@ -3,7 +3,7 @@
 namespace Sanlog.Formatters
 {
     /// <summary>
-    /// Defines a method that supports custom formatting of the value of an object.
+    /// Defines methods that supports custom formatting of the object.
     /// </summary>
     public interface IValueFormatter : ICustomFormatter, IFormatProvider { }
 }
